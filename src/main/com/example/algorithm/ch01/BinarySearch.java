277ae6eec943d2443d3c1d77f90531847509ed33
Wusung Peng <wusung.peng@kyper.com>
2015-10-13 23:27:20 +0800

    Add recurisve binarySearch()

--- src/main/com/example/algorithm/ch01/BinarySearch.java
+++ src/main/com/example/algorithm/ch01/BinarySearch.java
@@ -20,4 +20,22 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    public static int rank2(int key, int a[]) {
+        return rank2(key, a, 0, a.length - 1);
+    }
+
+    private static int rank2(int key, int[] a, int lo, int hi) {
+        if (lo > hi) {
+            return -1;
+        }
+        int mid = lo + (hi - lo) / 2;
+        if (key < a[mid]) {
+            return rank2(key, a, lo, mid - 1);
+        } else if (key > a[mid]) {
+            return rank2(key, a, mid + 1, hi);
+        } else {
+            return mid;
+        }
+    }
 }
--- src/test/com/example/algorithm/ch01/BinarySearchTest.java
+++ src/test/com/example/algorithm/ch01/BinarySearchTest.java
@@ -15,4 +15,10 @@ public class BinarySearchTest extends TestCase {
         assertEquals(4, BinarySearch.rank(5, new int[]{1, 2, 3, 4, 5}));
         assertEquals(-1, BinarySearch.rank(10, new int[]{1, 2, 3, 4, 5}));
     }
+
+    public void testRank2() throws Exception {
+        assertEquals(3, BinarySearch.rank2(4, new int[]{1, 2, 3, 4, 5}));
+        assertEquals(4, BinarySearch.rank2(5, new int[]{1, 2, 3, 4, 5}));
+        assertEquals(-1, BinarySearch.rank2(10, new int[]{1, 2, 3, 4, 5}));
+    }
 }
